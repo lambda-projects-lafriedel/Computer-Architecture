311ff23bf157c562997c46e69fd7bc8379ece6cf
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-07-23 10:34:09 -0400

    Step 1: Initialize cpu.py constructor

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,8 +6,10 @@ class CPU:
     """Main CPU class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256 # base 10 indexing
+        self.reg = [0] * 8
+        self.pc = 0
+        
 
     def load(self):
         """Load a program into memory."""
